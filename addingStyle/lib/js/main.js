22fda177717356cad78c865ac8d384596a51cf5e
Ryan Samsonov <ryansamsonov@gmail.com>
2018-07-31 10:04:40 -0400

    add first two projects

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("right_side")[0].style.background = "#d3c26f"
+const namesArray = document.getElementsByClassName("name")
+namesArray[0].style.textShadow = "-6 5px 2px yellow";
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", onClick);
+
+function onClick(){
+    document.getElementById("rengar").src="assets/rengar.png";
+    alert("You've been deleted!");
+}
